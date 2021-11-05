231747053bf5ec0af433835b58f7dbd0589beae9
Anilhwan <dlfghks0922@naver.com>
2021-11-05 18:18:29 +0900

    swiper

--- mobile/js/script.js
+++ mobile/js/script.js
@@ -13,3 +13,31 @@ $('.menu-box').click(function(){
    $(this).toggleClass('add')
    $('.side-menu').toggleClass('slide')
 })
+
+// 메인페이지 스와이퍼
+var swiper = new Swiper(".mySwiper", {
+   navigation: {
+     nextEl: ".button-next",
+     prevEl: ".button-prev",
+   },
+   loop:true,
+   autoplay:{
+		delay:5000,   /* 1초마다 오토플레이 */
+		disableOnInteraction: false,		/* false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음 */
+	},
+ });
+//  스와이퍼 재생 멈춤
+var btn =$('.graph').find('button')
+var progress =$('.bar')
+
+btn.click(function(){
+   if(btn.hasClass('play')){
+      swiper.autoplay.stop();
+      $(this).removeClass('play');
+      $(this).text('재생');
+   }else{
+      swiper.autoplay.start();
+      $(this).addClass('play');
+      $(this).text('정지');
+      }
+});
