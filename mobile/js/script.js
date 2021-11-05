ab4fd1adf20cfa5bbbfb3d338eace48aafaf167a
Anilhwan <dlfghks0922@naver.com>
2021-11-05 21:54:18 +0900

    progressbar

--- mobile/js/script.js
+++ mobile/js/script.js
@@ -14,6 +14,9 @@ $('.menu-box').click(function(){
    $('.side-menu').toggleClass('slide')
 })
 
+var btn =$('.graph').find('button')
+var progress =$('.bar')
+progress.animate({width:'100%'},5000);
 // 메인페이지 스와이퍼
 var swiper = new Swiper(".mySwiper", {
    navigation: {
@@ -22,22 +25,26 @@ var swiper = new Swiper(".mySwiper", {
    },
    loop:true,
    autoplay:{
-		delay:5000,   /* 1초마다 오토플레이 */
-		disableOnInteraction: false,		/* false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음 */
-	},
+      delay:5000,   /* 1초마다 오토플레이 */
+      disableOnInteraction: false,      /* false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음 */
+   },
+   on:{
+      slideChange:()=>{
+         progress.stop().css({width:0}).animate({width:'100%'},5000);
+      }
+   }
  });
-//  스와이퍼 재생 멈춤
-var btn =$('.graph').find('button')
-var progress =$('.bar')
-
+//  재생 정지버튼
 btn.click(function(){
    if(btn.hasClass('play')){
       swiper.autoplay.stop();
       $(this).removeClass('play');
       $(this).text('재생');
+      progress.stop().animate({width:0})
    }else{
       swiper.autoplay.start();
       $(this).addClass('play');
       $(this).text('정지');
-      }
-});
+      progress.animate({width:'100%'},5000);
+   }
+})
