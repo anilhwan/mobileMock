49d647b7eb0febdfab5bd2bc776fb4977876d3b6
Anilhwan <dlfghks0922@naver.com>
2021-11-17 01:33:02 +0900

    mapdd

--- mobile/js/store.js
+++ mobile/js/store.js
@@ -41,60 +41,34 @@ $(window).scroll(function(){
 });
 
 // 지도
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-                  mapOption = {
-                      center: new kakao.maps.LatLng(37.56682, 126.97865), // 지도의 중심좌표
-                      level: 3, // 지도의 확대 레벨
-                      mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
-                  }; 
-// 지도를 생성한다 
-var map = new kakao.maps.Map(mapContainer, mapOption);
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        var options = { //지도를 생성할 때 필요한 기본 옵션
+            center: new kakao.maps.LatLng(37.26802307605828, 127.00555176387485), //지도의 중심좌표.
+            level: 3 //지도의 레벨(확대, 축소 정도)
+        };
+        var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        var mapTypeControl = new kakao.maps.MapTypeControl();
 
-function locationLoadSuccess(pos){
-   // 현재 위치 받아오기
-   var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+        // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+        // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+        map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-   // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
-   map.panTo(currentPos);
-
-   // 마커 생성
-   var marker = new kakao.maps.Marker({
-         position: currentPos
-   });
-
-   // 기존에 마커가 있다면 제거
-   marker.setMap(null);
-   marker.setMap(map);
-};
-
-function locationLoadError(pos){
-   alert('위치 정보를 가져오는데 실패했습니다.');
-};
-
-// 위치 가져오기 버튼 클릭시
-function getCurrentPosBtn(){
-   navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
-};
-function setMapType(maptype) { 
-   var roadmapControl = document.getElementById('btnRoadmap');
-   var skyviewControl = document.getElementById('btnSkyview'); 
-   if (maptype === 'roadmap') {
-       map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);    
-       roadmapControl.className = 'selected_btn';
-       skyviewControl.className = 'btn';
-   } else {
-       map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);    
-       skyviewControl.className = 'selected_btn';
-       roadmapControl.className = 'btn';
-   }
-}
-
-// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-function zoomIn() {
-   map.setLevel(map.getLevel() - 1);
-}
-
-// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
-function zoomOut() {
-   map.setLevel(map.getLevel() + 1);
-}
+        // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+        var zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+        
+        function locationLoadSuccess(pos){
+         // 현재 위치 받아오기
+         var currentPos = new kakao.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+      
+         // 지도 이동(기존 위치와 가깝다면 부드럽게 이동)
+         map.panTo(currentPos);
+      };
+      
+      function locationLoadError(pos){
+         alert('위치 정보를 가져오는데 실패했습니다.');
+      };
+      // 위치 가져오기 버튼 클릭시
+      function getCurrentPosBtn(){
+         navigator.geolocation.getCurrentPosition(locationLoadSuccess,locationLoadError);
+      };
